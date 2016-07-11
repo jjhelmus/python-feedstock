b085cce8641a40678f3db32f6fb1ce56e8e736bd
Jonathan J. Helmus <jjhelmus@gmail.com>
2016-07-11 18:36:28 -0500

    WIP: testing with SDK

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,21 +8,20 @@ REM Compile python, extensions and external libraries
 if "%ARCH%"=="64" (
    set PLATFORM=x64
    set VC_PATH=x64
-   set PCB=-%SRC_DIR%\PC\VS9.0\amd64
+   set PCB=%SRC_DIR%\PCbuild\amd64
    :: Next line is only for local builds
-   :: call "C:\Users\filler\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" x64
+   call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat"
 ) else (
    set PLATFORM=Win32
    set VC_PATH=x86
-   set PCB=-%SRC_DIR%\PC\VS9.0
+   set PCB=%SRC_DIR%\PCbuild
+   call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 )
 
-cd PC
-cd VS9.0
+cd PCbuild
 dir
 call build.bat -e -p %PLATFORM%
 if errorlevel 1 exit 1
-cd ..
 cd ..
 
 
@@ -52,101 +51,101 @@ if errorlevel 1 exit 1
 copy /Y %PCB%\tk86t.dll %PREFIX%\DLLs\
 if errorlevel 1 exit 1
 
-copy /Y %SRC_DIR%\PC\py.ico %PREFIX%\DLLs\
-if errorlevel 1 exit 1
-copy /Y %SRC_DIR%\PC\pyc.ico %PREFIX%\DLLs\
-if errorlevel 1 exit 1
+::copy /Y %SRC_DIR%\PC\py.ico %PREFIX%\DLLs\
+::if errorlevel 1 exit 1
+::copy /Y %SRC_DIR%\PC\pyc.ico %PREFIX%\DLLs\
+::if errorlevel 1 exit 1
 
 
-REM Populate the Tools directory
-mkdir %PREFIX%\Tools
-xcopy /s /y /i %SRC_DIR%\Tools\demo %PREFIX%\Tools\demo
-if errorlevel 1 exit 1
-xcopy /s /y /i %SRC_DIR%\Tools\i18n %PREFIX%\Tools\i18n
-if errorlevel 1 exit 1
-xcopy /s /y /i %SRC_DIR%\Tools\parser %PREFIX%\Tools\parser
-if errorlevel 1 exit 1
-xcopy /s /y /i %SRC_DIR%\Tools\pynche %PREFIX%\Tools\pynche
-if errorlevel 1 exit 1
-xcopy /s /y /i %SRC_DIR%\Tools\scripts %PREFIX%\Tools\scripts
-if errorlevel 1 exit 1
+::REM Populate the Tools directory
+::mkdir %PREFIX%\Tools
+::xcopy /s /y /i %SRC_DIR%\Tools\demo %PREFIX%\Tools\demo
+::if errorlevel 1 exit 1
+::xcopy /s /y /i %SRC_DIR%\Tools\i18n %PREFIX%\Tools\i18n
+::if errorlevel 1 exit 1
+::xcopy /s /y /i %SRC_DIR%\Tools\parser %PREFIX%\Tools\parser
+::if errorlevel 1 exit 1
+::xcopy /s /y /i %SRC_DIR%\Tools\pynche %PREFIX%\Tools\pynche
+::if errorlevel 1 exit 1
+::xcopy /s /y /i %SRC_DIR%\Tools\scripts %PREFIX%\Tools\scripts
+::if errorlevel 1 exit 1
 
-del %PREFIX%\Tools\demo\README
-if errorlevel 1 exit 1
-del %PREFIX%\Tools\pynche\README
-if errorlevel 1 exit 1
-del %PREFIX%\Tools\pynche\pynche
-if errorlevel 1 exit 1
-del %PREFIX%\Tools\scripts\README
-if errorlevel 1 exit 1
-del %PREFIX%\Tools\scripts\dutree.doc
-if errorlevel 1 exit 1
-del %PREFIX%\Tools\scripts\idle3
-if errorlevel 1 exit 1
+::del %PREFIX%\Tools\demo\README
+::if errorlevel 1 exit 1
+::del %PREFIX%\Tools\pynche\README
+::if errorlevel 1 exit 1
+::del %PREFIX%\Tools\pynche\pynche
+::if errorlevel 1 exit 1
+::del %PREFIX%\Tools\scripts\README
+::if errorlevel 1 exit 1
+::del %PREFIX%\Tools\scripts\dutree.doc
+::if errorlevel 1 exit 1
+::del %PREFIX%\Tools\scripts\idle3
+::if errorlevel 1 exit 1
 
-move /y %PREFIX%\Tools\scripts\2to3 %PREFIX%\Tools\scripts\2to3.py
-if errorlevel 1 exit 1
-move /y %PREFIX%\Tools\scripts\pydoc3 %PREFIX%\Tools\scripts\pydoc3.py
-if errorlevel 1 exit 1
-move /y %PREFIX%\Tools\scripts\pyvenv %PREFIX%\Tools\scripts\pyvenv.py
-if errorlevel 1 exit 1
-
-
-REM Populate the tcl directory
-if "%ARCH%"=="64" (
-   xcopy /s /y /i %SRC_DIR%\externals\tcltk64\lib %PREFIX%\tcl
-   if errorlevel 1 exit 1
-) else (
-   xcopy /s /y /i %SRC_DIR%\externals\tcltk\lib %PREFIX%\tcl
-   if errorlevel 1 exit 1
-)
+::move /y %PREFIX%\Tools\scripts\2to3 %PREFIX%\Tools\scripts\2to3.py
+::if errorlevel 1 exit 1
+::move /y %PREFIX%\Tools\scripts\pydoc3 %PREFIX%\Tools\scripts\pydoc3.py
+::if errorlevel 1 exit 1
+::move /y %PREFIX%\Tools\scripts\pyvenv %PREFIX%\Tools\scripts\pyvenv.py
+::if errorlevel 1 exit 1
 
 
-REM Populate the include directory
-xcopy /s /y %SRC_DIR%\Include %PREFIX%\include\
-if errorlevel 1 exit 1
-
-copy /Y %SRC_DIR%\PC\pyconfig.h %PREFIX%\include\
-if errorlevel 1 exit 1
-
-
-REM Populate the Scripts directory
-IF NOT exist %SCRIPTS% (mkdir %SCRIPTS%)
-if errorlevel 1 exit 1
-
-for %%x in (idle pydoc) do (
-    copy /Y %SRC_DIR%\Tools\scripts\%%x3 %SCRIPTS%\%%x
-    if errorlevel 1 exit 1
-)
-
-copy /Y %SRC_DIR%\Tools\scripts\2to3 %SCRIPTS%
-if errorlevel 1 exit 1
+::REM Populate the tcl directory
+::if "%ARCH%"=="64" (
+::   xcopy /s /y /i %SRC_DIR%\externals\tcltk64\lib %PREFIX%\tcl
+::   if errorlevel 1 exit 1
+::) else (
+::   xcopy /s /y /i %SRC_DIR%\externals\tcltk\lib %PREFIX%\tcl
+::   if errorlevel 1 exit 1
+::)
 
 
-REM Populate the libs directory
-mkdir %PREFIX%\libs
-copy /Y %PCB%\python27.lib %PREFIX%\libs\
-if errorlevel 1 exit 1
-copy /Y %PCB%\python.lib %PREFIX%\libs\
-if errorlevel 1 exit 1
-copy /Y %PCB%\_tkinter.lib %PREFIX%\libs\
-if errorlevel 1 exit 1
+::REM Populate the include directory
+::xcopy /s /y %SRC_DIR%\Include %PREFIX%\include\
+::if errorlevel 1 exit 1
+
+::copy /Y %SRC_DIR%\PC\pyconfig.h %PREFIX%\include\
+::if errorlevel 1 exit 1
 
 
-REM Populate the Lib directory
-del %PREFIX%\libs\libpython*.a
-xcopy /s /y %SRC_DIR%\Lib %PREFIX%\Lib\
-if errorlevel 1 exit 1
+::REM Populate the Scripts directory
+::IF NOT exist %SCRIPTS% (mkdir %SCRIPTS%)
+::if errorlevel 1 exit 1
+
+::for %%x in (idle pydoc) do (
+::    copy /Y %SRC_DIR%\Tools\scripts\%%x3 %SCRIPTS%\%%x
+::    if errorlevel 1 exit 1
+::)
+
+::copy /Y %SRC_DIR%\Tools\scripts\2to3 %SCRIPTS%
+::if errorlevel 1 exit 1
 
 
-REM bytecode compile the standard library
+::REM Populate the libs directory
+::mkdir %PREFIX%\libs
+::copy /Y %PCB%\python27.lib %PREFIX%\libs\
+::if errorlevel 1 exit 1
+::copy /Y %PCB%\python.lib %PREFIX%\libs\
+::if errorlevel 1 exit 1
+::copy /Y %PCB%\_tkinter.lib %PREFIX%\libs\
+::if errorlevel 1 exit 1
 
-rd /s /q %STDLIB_DIR%\lib2to3\tests\
-if errorlevel 1 exit 1
 
-%PYTHON% -Wi %STDLIB_DIR%\compileall.py -f -q -x "bad_coding|badsyntax|py2_" %STDLIB_DIR%
-if errorlevel 1 exit 1
+::REM Populate the Lib directory
+::del %PREFIX%\libs\libpython*.a
+::xcopy /s /y %SRC_DIR%\Lib %PREFIX%\Lib\
+::if errorlevel 1 exit 1
 
 
-REM Pickle lib2to3 Grammar
-%PYTHON% -m lib2to3 --help
+::REM bytecode compile the standard library
+
+::rd /s /q %STDLIB_DIR%\lib2to3\tests\
+::if errorlevel 1 exit 1
+
+::%PYTHON% -Wi %STDLIB_DIR%\compileall.py -f -q -x "bad_coding|badsyntax|py2_" %STDLIB_DIR%
+::if errorlevel 1 exit 1
+
+
+::REM Pickle lib2to3 Grammar
+::%PYTHON% -m lib2to3 --help
