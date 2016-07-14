ec1daea388fcfb6b0353a4094bb879af16a395b0
Jonathan Helmus <jjhelmus@gmail.com>
2016-07-14 08:30:35 -0500

    pydoc3 -> pydoc

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -85,7 +85,7 @@ if errorlevel 1 exit 1
 
 move /y %PREFIX%\Tools\scripts\2to3 %PREFIX%\Tools\scripts\2to3.py
 if errorlevel 1 exit 1
-move /y %PREFIX%\Tools\scripts\pydoc3 %PREFIX%\Tools\scripts\pydoc.py
+move /y %PREFIX%\Tools\scripts\pydoc %PREFIX%\Tools\scripts\pydoc.py
 if errorlevel 1 exit 1
 
 
