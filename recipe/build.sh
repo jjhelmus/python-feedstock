18f322d02620815e11d922fa53b358f6ea939b21
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-13 11:20:51 -0600

    Strip WIP

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 python ${RECIPE_DIR}/brand_python.py
 
+rm -rf Lib/test Lib/*/test
+rm -rf Lib/ensurepip
+
 if [ `uname` == Darwin ]; then
     export CFLAGS="-I$PREFIX/include $CFLAGS"
     export LDFLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -headerpad_max_install_names $LDFLAGS"
@@ -19,11 +22,12 @@ if [ `uname` == Linux ]; then
         --prefix=$PREFIX \
         --with-tcltk-includes="-I$PREFIX/include" \
         --with-tcltk-libs="-L$PREFIX/lib -ltcl8.5 -ltk8.5" \
+        CFLAGS="-g0"    \
         CPPFLAGS="-I$PREFIX/include" \
-        LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
+        LDFLAGS="-s -L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
 fi
 
-make
+make -j6
 make install
 ln -s $PREFIX/bin/python3.5 $PREFIX/bin/python
 ln -s $PREFIX/bin/pydoc3.5 $PREFIX/bin/pydoc
